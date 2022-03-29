3eaedab478cf6f1f34a222c0aceed59d172c4785
Mejsun <mejsunalghoul@gmail.com>
2022-03-29 16:39:03 +0100

    mapping projects

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -1,72 +1,47 @@
 import React from 'react'
-import lang from '../static/Images/Lang.png'
-import calc from '../static/Images/calc.png'
-import pokedex from '../static/Images/Pokedex.png'
-import skynews from '../static/Images/skynews.png'
-import underconstr from '../static/Images/underconstr.png'
 
 function Projects() {
-  return (
-    <div>
-    <h1>Projects</h1>
-    <div className='container'>
-      <div className='projects' >
-        <div className='projects-cell'>
-          <img src={underconstr} alt='memento'/>
-          <div className='projectText'>
-            <h2 href='https://github.com/Mejsun/wishlist'>Wishlist</h2>
-            <p> Tech used: React, API’s, React Hooks, Bootstrap, Sass</p>
-            <p> Changes pending:</p>
-            <p>- Adding sign up / log in functionality</p> 
-          </div>
-        </div>
-      
-        <div className='projects-cell'>
-        <img src={calc} alt='calculate'/>
-          <div className='projectText'>
-            <h2 href='https://calculatingaway.netlify.app/' target='_blank'>Calculator</h2>
-            <p>Tech used: HTML, CSS, Javascript</p>
-          </div>
-        </div>
-        
-        <div className='projects-cell'>
-          <img src={underconstr} alt='under construction'/>
-          <div className='projectText'>
-            <h2 href='https://github.com/Mejsun/urlShortener' target='_blank'>URL shortener</h2>
-            <p>Tech used: JavaScript, MongoDB, ExpressJS</p>
-            <p> Changes pending:</p>
-            <p> - Create the front-end side of application</p>
-          </div>
-        </div>
-        
-        <div className='projects-cell'>
-          <img src={pokedex} alt='pokedex'/>
-          <div className='projectText'>
-            <h2 href='https://mejsunspokedex.netlify.app/' target='_blank'>Pokedex</h2>
-            <p>Tech used: HTML, CSS, JavaScript, API’s </p>
-          </div>
-        </div>
-        
-        <div className='projects-cell'>
-          <img src={lang} alt='language shake'/>
-          <div className='projectText'>
-            <h2 href='https://languageshake.netlify.app/' target='_blank'>Language shake</h2>
-            <p>Tech used: HTML, CSS, JavaScript</p>
-          </div>
-        </div>
-        
-        <div className='projects-cell'>
-          <img src={skynews} alt='skynews'/>
-          <div className='projectText'>
-            <h2 href='https://skynewsremake.netlify.app/' target='_blank'>Sky News remake</h2>
-            <p>Tech used: HTML, CSS </p>
-          </div>
-        </div>
-      </div>
-    </div>
-    <button className='prev'><i className='fas fa-angle-left'></i> </button>
-    <button className='next'><i className='fas fa-angle-right'></i> </button>
 
+const projectsList = [
+  {name: 'Wishlist', tech: 'React, API’s, React Hooks, Bootstrap, Sass', 
+  bgr: 'underconstr', link: 'https://github.com/Mejsun/wishlist'},
+  {name: 'Calculator', tech: 'React, API’s, React Hooks, Bootstrap, Sass', 
+  bgr: 'calc', link: 'https://github.com/Mejsun/wishlist'},
+  {name: 'URL Shortener', tech: 'React, API’s, React Hooks, Bootstrap, Sass', 
+  bgr: 'underconstr', link: 'https://github.com/Mejsun/wishlist'},
+  {name: 'Pokedex', tech: 'React, API’s, React Hooks, Bootstrap, Sass', 
+  bgr: 'Pokedex', link: 'https://github.com/Mejsun/wishlist'},
+  {name: 'Language Shake', tech: 'React, API’s, React Hooks, Bootstrap, Sass', 
+  bgr: 'skynews', link: 'https://github.com/Mejsun/wishlist'},
+  {name: 'SkyNews', tech: 'React, API’s, React Hooks, Bootstrap, Sass', 
+  bgr:'Lang', link: 'https://github.com/Mejsun/wishlist'},
+]
+
+
+function next() {
+  console.log('next')
+}
+function prev() {
+  //projectIndex--
+  console.log('prev')
+}
+
+  return (
+    <div className='wrapper'>
+    <h1>Projects</h1>
+      <div className='projects' >
+      {projectsList.map((project, index) => (
+          <div className='projects-cell' key={index}>
+            <img src={require('../static/Images/' + project.bgr + '.png')} alt='screenshot'/>
+            <div className='projectText'>
+              <a className='heading2' href={project.link} target='_blank' rel='noreferrer'>{project.name}</a>
+              <p> Tech used: {project.tech} </p>
+            </div>
+          </div>
+      ))}
+      </div>
+    <button className='prev' onClick={prev}><i className='fas fa-angle-left'></i> </button>
+    <button className='next' onClick={next}><i className='fas fa-angle-right'></i> </button>
     </div>
   )
 }
