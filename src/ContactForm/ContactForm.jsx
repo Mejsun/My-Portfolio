e885e2422db458d2f4734ec8a166ce4796c99076
Mejsun <mejsunalghoul@gmail.com>
2022-03-30 16:08:33 +0100

    contact form functionality and basic styling

--- src/ContactForm/ContactForm.jsx
+++ src/ContactForm/ContactForm.jsx
@@ -1,30 +1,102 @@
-import React from 'react'
+import React, {useRef, useState} from 'react'
 
 function ContactForm() {
+
+  const [contact, setContact] = useState({name:'', email:'', message:''});
+const nameRef = useRef();
+const emailRef = useRef();
+const messageRef = useRef();
+
+function collectInfo(){
+  contact.name = nameRef.current.value
+  contact.email = emailRef.current.value
+  contact.message = messageRef.current.value
+  validateInfo()
+}
+
+function validateInfo(){
+  const emailRegex = /^[a-zA-Z0-9!#$%&'*+=?^_`{|}~\W]+@[a-zA-Z0-9!#$%&'*+=?^_`{|}~\W]+\.[a-z.]{2,}/gm
+  const nameRegex = /^[^±!@£$%^&*_+§¡€#¢§¶•ªº«\\<>?:;|=.,0-9]{2,}$/gm
+
+  if (contact.name && nameRegex.test(contact.name) === true){
+    nameRef.current.className = 'valid'
+    console.log('Name valid!');
+  }else{
+    nameRef.current.className = 'invalid'
+    console.log('Name invalid!');
+  }
+
+  if (contact.email && emailRegex.test(contact.email) === true){
+    emailRef.current.className = 'valid'
+    console.log('email valid!')
+  }else{
+    emailRef.current.className = 'invalid'
+    console.log('email error!')
+  }
+
+  if(contact.message === ''){ 
+    messageRef.current.className ='invalid'
+    console.log('msg error!')
+  }else{
+    messageRef.current.className = 'valid'
+    console.log('msg ok!')
+  };
+}
+
+function contactInfo (e){
+  setContact((info) => ({
+    ...info,
+    [e.target.name]: e.target.value,
+  }));
+}
+
+async function submitForm (){
+  if(contact.name && contact.email && contact.message){
+      const res = fetch("https://mejsunsnodemailer.herokuapp.com/contact", {
+      method: "POST",
+      headers: {"Content-type": "application/json",},
+      body: JSON.stringify ({contact}),
+    })
+     .then((res) => res.json())
+     .then(async (res) => {
+      const resData = await res;
+      console.log(resData);
+      if (resData.status === "success") {
+        alert("Thank you!");
+        nameRef.current.className = 'valid'
+        emailRef.current.className = 'valid'
+        messageRef.current.className = 'valid'
+      } else if (resData.status === "fail") {
+        alert("Message failed to send");
+      }
+    })
+  } 
+}
+
   return (
-    <div className='wrapper'>
+    <div className='contact-container wrapper'>
     <h1>Get in touch</h1>
     <form>
       <label htmlFor='name'>Name </label>
       <input type='text' name='name'
-      //onChange={(e) => {contactInfo(e)}} 
-      //onBlur={()=>collectInfo()} ref={nameRef} 
+      onChange={(e) => {contactInfo(e)}} 
+      onBlur={()=>collectInfo()} ref={nameRef} 
       />
       
       <label htmlFor='email'> Email </label>
       <input type='email' name='email' 
-      //onChange={(e) => {contactInfo(e)}} 
-      //onBlur={()=>collectInfo()} ref={emailRef} 
+      onChange={(e) => {contactInfo(e)}} 
+      onBlur={()=>collectInfo()} ref={emailRef} 
       />
       
       <label htmlFor='message'> Message </label>
       <input className='message' type='text' name='message' 
-      //onChange={(e) => {contactInfo(e)}}
-      //onBlur={()=>collectInfo()} ref={messageRef} 
+      onChange={(e) => {contactInfo(e)}}
+      onBlur={()=>collectInfo()} ref={messageRef} 
       />
       
       <button type='button' 
-      //onClick={() => submitForm()}
+      onClick={() => submitForm()}
       >Submit</button>
     </form>
     <div className='contact-grid'>
