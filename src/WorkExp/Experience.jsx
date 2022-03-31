72bffa51d76b273458cedc632da3760acada4245
Mejsun <mejsunalghoul@gmail.com>
2022-03-31 17:03:08 +0100

    work section styling

--- src/WorkExp/Experience.jsx
+++ src/WorkExp/Experience.jsx
@@ -9,24 +9,24 @@ function Experience () {
             Flower animation
         </div>
       </div>
-        <div>
-          <div>
-            <h2>04/2017 –  08/2018</h2>  
-            <h2>International Relations Office, University of Sarajevo - Intern</h2> 
+        <div className='work-experience-list'>
+          <div className='work-experience-item'>
+            <h2 className='date'>04/2017 –  08/2018</h2>  
+            <h2 className='position'>International Relations Office, University of Sarajevo: Intern</h2> 
             <p>- Preparing brochures, infographics, reports and managing and publishing online content</p>
             <p>- Researching information on visits, cooperation initiatives and agreements </p>
             <p>- Organising various events for international staff and students</p>
           </div>
-          <div>
-            <h2>02 – 06/2019</h2>  
-            <h2>Wifirst UK - Marketing manager intern, London</h2>
+          <div className='work-experience-item'>
+            <h2 className='date'>02 – 06/2019</h2>  
+            <h2 className='position'>Wifirst UK: Marketing manager intern, London</h2>
             <p>- Creating, publishing and managing social media content </p>
             <p>- Planning and implementing promotional campaigns </p>
             <p>- Analysing potential strategic partner relationships</p>
           </div>
-          <div>
-            <h2>08/19 – Currently</h2>
-            <h2>Saudi Arabian Airlines - Executive Assistant to Regional Manager </h2>
+          <div className='work-experience-item'>
+            <h2 className='date'>08/19 – Currently</h2>
+            <h2 className='position'>Saudi Arabian Airlines: Executive Assistant to Regional Manager </h2>
             <p>- Main point of contact </p>
             <p>- Troubleshooting and problem solving</p>
             <p>- Writing and preparing infographics and reports</p>
