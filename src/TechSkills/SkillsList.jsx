cf6c2d4d9ceb9d92f0869b59f4ecee1333b3e5a5
Mejsun Alghoul <mejsunalghoul@gmail.com>
2024-07-03 16:08:20 +0200

    fix: update skills list

--- src/TechSkills/SkillsList.jsx
+++ src/TechSkills/SkillsList.jsx
@@ -3,23 +3,39 @@ import './Skills.scss'
 import Certificates from './Certificates';
 
 let skillsList = [
-  {name: 'HTML', icon: 'html5'},
+  {name: 'HTML', icon: 'html'},
   {name: 'CSS', icon: 'css'},
   {name: 'JavaScript', icon: 'js'},
+
   {name: 'React', icon: 'react'},
+  {name: 'Next.js', icon: 'nextjs'},
+  {name: 'Typescript', icon: 'ts'},
+
+  {name: 'Cypress', icon: 'cypress'},
+
   {name: 'Sass', icon: 'sass'},
-  {name: 'Bootstrap', icon: 'bootstrap'},
-  {name: 'APIs', icon: 'api'},
-  {name: 'Github', icon: 'github'},
   {name: 'Styled Components', icon: 'styled'},
+  {name: 'Tailwind', icon: 'tailwind'},
+  {name: 'Bootstrap', icon: 'bootstrap'},
+
+  {name: 'Storybook', icon: 'storybook'},
+  {name: 'Figma', icon: 'figma'},
   {name: 'Inkscape', icon: 'inkscape'},
-  {name: 'jQuery', icon: 'jquery'},
-  {name: 'MongoDB', icon: 'mongodb'},
-  {name: 'Node.js', icon: 'nodejs'},
-  {name: 'Canva', icon: 'canva'},
-  {name: 'Wordpress', icon: 'wordpress'},
   {name: 'Adobe XD', icon: 'adobexd'},
-  {name: 'JSON', icon: 'json'},
+  {name: 'Canva', icon: 'canva'},
+
+  // {name: 'jQuery', icon: 'jquery'},
+  // {name: 'JSON', icon: 'json'},
+
+  {name: 'MySql', icon: 'mysql'},
+  {name: 'MongoDB', icon: 'mongodb'},
+
+  {name: 'Node.js', icon: 'nodejs'},
+  {name: 'APIs', icon: 'api'},
+
+  {name: 'Github', icon: 'github'},
+  // {name: 'Wordpress', icon: 'wordpress'},
+  {name: 'Zapier', icon: 'zapier'},
 ]
 
 function SkillsList() {
