226c55a382341b9246d47225bb928f27947669a7
Mejsun Alghoul <mejsunalghoul@gmail.com>
2024-07-03 15:54:25 +0200

    fix: update certficates view

--- src/TechSkills/Certificates.jsx
+++ src/TechSkills/Certificates.jsx
@@ -5,21 +5,13 @@ function Certificates() {
     <div>
     <h1>Certificates</h1>
     <div className='certificates'>
-  
-        <div className='certificate-text'>
-          <h2>FreeCodeCamp</h2>
-          <p>Responsive Web Design (HTML and CSS)</p>
-        </div>
-      
-        <div className='certificate-text'>
-          <h2>FreeCodeCamp</h2>
-          <p>JavaScript Algorithms and Data Structures</p>
-        </div>
-      
-        <div className='certificate-text'>
-          <h2>FreeCodeCamp</h2>
-          <p>Front End Libraries (Bootstrap, jQuery, Sass, React, Redux)</p>
-         </div>
+
+        <h2>FreeCodeCamp</h2>
+        <ul>
+          <li className='certificate-text'>Responsive Web Design (HTML and CSS)</li>
+          <li className='certificate-text'>JavaScript Algorithms and Data Structures</li>
+          <li className='certificate-text'>Front End Libraries (Bootstrap, jQuery, Sass, React, Redux)</li>
+         </ul>
       </div>
     </div>
   )
